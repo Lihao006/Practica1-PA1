4663a768904de8e91a2796d895a2e3fbc9a8a9be
Lihao006 <lihao.jin@estudiantat.upc.edu>
2024-12-22 19:18:03 +0100

    Update abs_board.py

--- abs_board.py
+++ abs_board.py
@@ -151,6 +151,10 @@ def set_board_up(stones_per_player = 4):
                     played_stones2.append(Stone(i, j, PLAYER_COLOR[0]))
                     turn = 1
                     return stone_selected, turn, end()
+            else:
+                print ("La casella no és vàlida o està ocupada.")
+                print ("Introdueix una casella vàlida.")
+                return bool(len(played_stones2) < stones_per_player), turn, end()
         else:
             if selected_stone != None:
                 si = selected_stone[0]
